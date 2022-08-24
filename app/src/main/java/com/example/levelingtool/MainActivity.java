f6e363dbc2f43db3da8ef008952fcedeca90a7ac
filda <filda.navratil@gmail.com>
2022-08-24 17:27:43 +0200

    final version I used for leveling my parcel

--- app/src/main/java/com/example/levelingtool/MainActivity.java
+++ app/src/main/java/com/example/levelingtool/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.levelingtool;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    TextView textViewResult, textViewResults;
+    EditText editTextFloor, editTextMeasured;
+    double floor, measured, result;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
+    String resultS, resultsS;
+    int counter=0;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // using toolbar as ActionBar
+        super.setRequestedOrientation(
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        textViewResult = (TextView)findViewById(R.id.textViewResult);
+        textViewResults = (TextView)findViewById(R.id.textViewResults);
+        editTextFloor = (EditText)findViewById(R.id.editTextFloor);
+        editTextMeasured = (EditText)findViewById(R.id.editTextMeasured);
+        button = (Button)findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                counter++;
+                floor = Double.parseDouble(editTextFloor.getText().toString());
+                measured = Double.parseDouble(editTextMeasured.getText().toString());
+                result = floor - measured;
+                resultS = df.format(result);
+                textViewResult.setText(resultS);
+                resultsS = textViewResults.getText().toString();
+                resultsS += "\n"+counter+" point: "+resultS;
+                textViewResults.setText(resultsS);
+            }
+        });
     }
 }
